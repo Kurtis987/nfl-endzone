483f7fc16f5af8316d2915a3bc496d3a056922f2
Kurtis Lunz <Kurtis987@gmail.com>
2018-04-08 17:29:44 -0400

    Added Tweets to home page

--- app/nfl-endzone.py
+++ app/nfl-endzone.py
@@ -2,6 +2,7 @@ import json
 import tweepy
 from tweepy import OAuthHandler
 import config
+from flask import Flask, abort, render_template
 
 
 auth = OAuthHandler(config.CONSUMER_KEY, config.CONSUMER_SECRET)
@@ -12,20 +13,26 @@ api = tweepy.API(auth)
 count = 4
 query = 'NFL'
 
-# Get all status
-results = [status for status in tweepy.Cursor(api.search, q=query).items(count)]
-
-#for result in results:
-#    print json.dumps(result._json, indent=2)
 
 # Get all status
 results = [status for status in tweepy.Cursor(api.search, q=query).items(count)]
+
 print(json.dumps(results[0]._json, indent=4))
 
 for status in results:
+    print("Divider Separator")
     print(status.text.encode('utf-8'))
     print(status.user.id)
     print(status.user.screen_name)
     print(status.user.profile_image_url_https)
     print(status.user.followers_count)
     print(status.place)
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html', results=results)
+if __name__ == '__main__':
+    app.run(debug=True)
