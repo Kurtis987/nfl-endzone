802b65d5eeba85142a96b85f8a48e6a96e654a1a
Kurtis Lunz <Kurtis987@gmail.com>
2018-05-03 16:44:54 -0400

    finished drawing charts

--- app/static/js/graph.js
+++ app/static/js/graph.js
@@ -3,30 +3,10 @@ queue()
     .await(makeGraphs);
 
 function makeGraphs(error, stats) {
-    console.log(stats);
     if (error) {
         console.error("makeGraphs error on receiving dataset:", error.statusText);
         throw error;
     }
-
-    /*var myData = [ 10, 15, 20, 30, 50 ];
-    var svg = d3.select("#stats-row-chart")
-                .append("svg")
-                .attr("width",750)
-                .attr("height",500)
-                .attr("style", "background-color:#ddd");
-
-    svg.selectAll("rect")
-        .data(myData)
-        .enter()
-        .append("rect")
-        .attr("x", function(d,i){return i*(50+ 10)})
-        .attr("y", function(d){return 500- d * 5;})//height is 500
-        .attr("width", 50)
-        .attr("height", function(d){return d * 5;})
-        .attr("fill", "#30C5C7"); */
-
-
 
     //Create a Crossfilter instance
     var ndx = crossfilter(stats);
@@ -53,18 +33,6 @@ function makeGraphs(error, stats) {
 
 
 
-
-    //Calculate metrics
-    var numByIndividualPassing = individualPassingDim.group();
-    var numByIndividualRushing = individualRushingDim.group();
-    var numByIndividualReceiving = individualReceivingDim.group();
-    var numByIndividualInterceptions = individualInterceptionsDim.group();
-    var numByIndividualSacks = individualSacksDim.group();
-    var numByIndividualTackles = individualTacklesDim.group();
-
-
-
-
     var topPassers = individualPassingDim.top(5);
     var topRushers = individualRushingDim.top(5);
     var topReceivers = individualReceivingDim.top(5);
@@ -72,431 +40,111 @@ function makeGraphs(error, stats) {
     var topSacks = individualSacksDim.top(5);
     var topTackles = individualTacklesDim.top(5);
 
-    //var width = 700,   // width of svg
-      //  height = 400;  // height of svg
-
-
-    /////////////////////////
-
-////////////////////////////////////////////////////////////////
-
-
-//passing chart
-// Dimensions
-var margin = {
-    top: 20,
-    right: 20,
-    bottom: 20,
-    left: 100
-},
-//width = parseInt(d3.select('#chart').style('width'), 10),
-    width = 500,
-    height = 500 - margin.top - margin.bottom,
-    barHeight = 40,
-    percent = d3.format('%');
-
-// Create the scale for the axis
-var xScale = d3.scale.linear()
-    .range([0, width]); // the pixel range to map to
-
-var yScale = d3.scale.ordinal()
-    .rangeRoundBands([190, height]);
-
-// Create the axis
-var xAxis = d3.svg.axis()
-    .scale(xScale)
-    .orient('bottom')
-    .ticks(5);
-
-var yAxis = d3.svg.axis()
-    .scale(yScale)
-    .orient('left');
-
-
-
-//passing chart
-xScale.domain([0, topPassers[0].passing]); // min/max extent of your data (this is usually dynamic e.g. max)
-//yScale.domain(stats.map(function (d) {
-yScale.domain(topPassers.map(function (d) {
-    console.log("name: " + d.name);
-    return d.name;
-}));
-
-// Render the SVG
-var svg = d3.select('#chart')
-    .append('svg')
-    .attr('height', height + margin.top + margin.bottom)
-    .attr('width', 650)
-    .append('g') // Group the content and add margin
-.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
-// Render the axis
-svg.append('g')
-    .attr('class', 'x axis')
-    .attr('transform', 'translate(0,' + height + ')')
-    .call(xAxis);
-
-svg.append("g")
-    .attr("class", "y axis")
-    .call(yAxis)
-    .selectAll(".tick text")
-    .call(wrap, yScale.rangeBand());
-
-// Render the bars
-var rect = svg.selectAll('rect')
-    .data(topPassers)
-    .enter();
-
-    rect.append('rect')
-    .attr('y', function (d) {
-    return yScale(d.name);})
-    .attr('width', function (d) {
-    return xScale(d.passing);})
-    .attr('height', barHeight);
-
-//place stats in rect
-rect.append("text")
-    .style("fill", "white")
-    .style("font-size", "14px")
-    .attr("dy", ".35em")
-    .attr("x",  function (d) {
-     return  xScale(d.passing)-40;})
-    .attr("y", function (d) {
-    return  yScale(d.name)+20;})
-    .style("style", "label")
-    .text(function(d) {
-    console.log("passing in rect: " +d.passing);
-    return d.passing; });
-
-svg.append("text")
-    .attr("x", (width / 2))
-    .attr("y", (margin.top *4))
-    .attr("text-anchor", "middle")
-    .style("font-size", "16px")
-    .style("text-decoration", "underline")
-    .text("Season Passing Yards");
-//end passing chart
-
-
-//start rushing chart
-xScale.domain([0, topRushers[0].rushing]); // min/max extent of your data (this is usually dynamic e.g. max)
-yScale.domain(topRushers.map(function (d) {
-    return d.name;
-}));
-
-// Render the SVG
-svg = d3.select('#rushing-chart')
-    .append('svg')
-    .attr('height', height + margin.top + margin.bottom)
-    .attr('width', 650)
-    .append('g') // Group the content and add margin
-.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
-// Render the axis
-svg.append('g')
-    .attr('class', 'x axis')
-    .attr('transform', 'translate(0,' + height + ')')
-    .call(xAxis);
-
-svg.append("g")
-    .attr("class", "y axis")
-    .call(yAxis)
-    .selectAll(".tick text")
-    .call(wrap, yScale.rangeBand());
-
-// Render the bars
-rect = svg.selectAll('rect')
-    .data(topRushers)
-    .enter();
-
-    rect.append('rect')
-    .attr('y', function (d) {
-    return yScale(d.name);})
-    .attr('width', function (d) {
-    return xScale(d.rushing);})
-    .attr('height', barHeight);
-
-//place stats in rect
-rect.append("text")
-    .style("fill", "white")
-    .style("font-size", "14px")
-    .attr("dy", ".35em")
-    .attr("x",  function (d) {
-     return  xScale(d.rushing)-40;})
-    .attr("y", function (d) {
-    return  yScale(d.name)+20;})
-    .style("style", "label")
-    .text(function(d) {
-    console.log(d.rushing);
-    return d.rushing; });
-
-svg.append("text")
-    .attr("x", (width / 2))
-    .attr("y", (margin.top *4))
-    .attr("text-anchor", "middle")
-    .style("font-size", "16px")
-    .style("text-decoration", "underline")
-    .text("Season Rushing Yards");
-
-//end rushing chart
-
-
-
-//receiving chart
-xScale.domain([0, topReceivers[0].receiving]); // min/max extent of your data (this is usually dynamic e.g. max)
-//yScale.domain(stats.map(function (d) {
-yScale.domain(topReceivers.map(function (d) {
-    console.log("name: " + d.name);
-    return d.name;
-}));
-
-// Render the SVG
-svg = d3.select('#receiving-chart')
-    .append('svg')
-    .attr('height', height + margin.top + margin.bottom)
-    .attr('width', 650)
-    .append('g') // Group the content and add margin
-.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
-// Render the axis
-svg.append('g')
-    .attr('class', 'x axis')
-    .attr('transform', 'translate(0,' + height + ')')
-    .call(xAxis);
-
-svg.append("g")
-    .attr("class", "y axis")
-    .call(yAxis)
-    .selectAll(".tick text")
-    .call(wrap, yScale.rangeBand());
-
-// Render the bars
-rect = svg.selectAll('rect')
-    .data(topReceivers)
-    .enter();
-
-    rect.append('rect')
-    .attr('y', function (d) {
-    return yScale(d.name);})
-    .attr('width', function (d) {
-    return xScale(d.receiving);})
-    .attr('height', barHeight);
-
-//place stats in rect
-rect.append("text")
-    .style("fill", "white")
-    .style("font-size", "14px")
-    .attr("dy", ".35em")
-    .attr("x",  function (d) {
-     return  xScale(d.receiving)-40;})
-    .attr("y", function (d) {
-    return  yScale(d.name)+20;})
-    .style("style", "label")
-    .text(function(d) {
-    console.log("receiving in rect: " +d.receiving);
-    return d.receiving; });
-
-svg.append("text")
-    .attr("x", (width / 2))
-    .attr("y", (margin.top *4))
-    .attr("text-anchor", "middle")
-    .style("font-size", "16px")
-    .style("text-decoration", "underline")
-    .text("Season Receiving Yards");
-//end receiving chart
-
-
-//interceptions chart
-xScale.domain([0, topInterceptions[0].interceptions]); // min/max extent of your data (this is usually dynamic e.g. max)
-//yScale.domain(stats.map(function (d) {
-yScale.domain(topInterceptions.map(function (d) {
-    console.log("name: " + d.name);
-    return d.name;
-}));
-
-// Render the SVG
-svg = d3.select('#interceptions-chart')
-    .append('svg')
-    .attr('height', height + margin.top + margin.bottom)
-    .attr('width', 650)
-    .append('g') // Group the content and add margin
-.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
-// Render the axis
-svg.append('g')
-    .attr('class', 'x axis')
-    .attr('transform', 'translate(0,' + height + ')')
-    .call(xAxis);
-
-svg.append("g")
-    .attr("class", "y axis")
-    .call(yAxis)
-    .selectAll(".tick text")
-    .call(wrap, yScale.rangeBand());
-
-// Render the bars
-rect = svg.selectAll('rect')
-    .data(topInterceptions)
-    .enter();
-
-    rect.append('rect')
-    .attr('y', function (d) {
-    return yScale(d.name);})
-    .attr('width', function (d) {
-    return xScale(d.interceptions);})
-    .attr('height', barHeight);
-
-//place stats in rect
-rect.append("text")
-    .style("fill", "white")
-    .style("font-size", "14px")
-    .attr("dy", ".35em")
-    .attr("x",  function (d) {
-     return  xScale(d.interceptions)-40;})
-    .attr("y", function (d) {
-    return  yScale(d.name)+20;})
-    .style("style", "label")
-    .text(function(d) {
-    console.log("interceptions in rect: " +d.interceptions);
-    return d.interceptions; });
-
-svg.append("text")
-    .attr("x", (width / 2))
-    .attr("y", (margin.top *4))
-    .attr("text-anchor", "middle")
-    .style("font-size", "16px")
-    .style("text-decoration", "underline")
-    .text("Season Interceptions");
-//end interceptions chart
-
-//sacks chart
-xScale.domain([0, topSacks[0].sacks]); // min/max extent of your data (this is usually dynamic e.g. max)
-//yScale.domain(stats.map(function (d) {
-yScale.domain(topSacks.map(function (d) {
-    console.log("name: " + d.name);
-    return d.name;
-}));
-
-// Render the SVG
-svg = d3.select('#sacks-chart')
-    .append('svg')
-    .attr('height', height + margin.top + margin.bottom)
-    .attr('width', 650)
-    .append('g') // Group the content and add margin
-.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
-// Render the axis
-svg.append('g')
-    .attr('class', 'x axis')
-    .attr('transform', 'translate(0,' + height + ')')
-    .call(xAxis);
-
-svg.append("g")
-    .attr("class", "y axis")
-    .call(yAxis)
-    .selectAll(".tick text")
-    .call(wrap, yScale.rangeBand());
-
-// Render the bars
-rect = svg.selectAll('rect')
-    .data(topSacks)
-    .enter();
-
-    rect.append('rect')
-    .attr('y', function (d) {
-    return yScale(d.name);})
-    .attr('width', function (d) {
-    return xScale(d.sacks);})
-    .attr('height', barHeight);
-
-//place stats in rect
-rect.append("text")
-    .style("fill", "white")
-    .style("font-size", "14px")
-    .attr("dy", ".35em")
-    .attr("x",  function (d) {
-     return  xScale(d.sacks)-40;})
-    .attr("y", function (d) {
-    return  yScale(d.name)+20;})
-    .style("style", "label")
-    .text(function(d) {
-    console.log("sacks in rect: " +d.sacks);
-    return d.sacks; });
-
-svg.append("text")
-    .attr("x", (width / 2))
-    .attr("y", (margin.top *4))
-    .attr("text-anchor", "middle")
-    .style("font-size", "16px")
-    .style("text-decoration", "underline")
-    .text("Season Sacks");
-//end sacks chart
-
-
-//tackles chart
-xScale.domain([0, topTackles[0].tackles]); // min/max extent of your data (this is usually dynamic e.g. max)
-//yScale.domain(stats.map(function (d) {
-yScale.domain(topTackles.map(function (d) {
-    console.log("name: " + d.name);
-    return d.name;
-}));
-
-// Render the SVG
-svg = d3.select('#tackles-chart')
-    .append('svg')
-    .attr('height', height + margin.top + margin.bottom)
-    .attr('width', 650)
-    .append('g') // Group the content and add margin
-.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
-// Render the axis
-svg.append('g')
-    .attr('class', 'x axis')
-    .attr('transform', 'translate(0,' + height + ')')
-    .call(xAxis);
-
-svg.append("g")
-    .attr("class", "y axis")
-    .call(yAxis)
-    .selectAll(".tick text")
-    .call(wrap, yScale.rangeBand());
-
-// Render the bars
-rect = svg.selectAll('rect')
-    .data(topTackles)
-    .enter();
-
-    rect.append('rect')
-    .attr('y', function (d) {
-    return yScale(d.name);})
-    .attr('width', function (d) {
-    return xScale(d.tackles);})
-    .attr('height', barHeight);
-
-//place stats in rect
-rect.append("text")
-    .style("fill", "white")
-    .style("font-size", "14px")
-    .attr("dy", ".35em")
-    .attr("x",  function (d) {
-     return  xScale(d.tackles)-40;})
-    .attr("y", function (d) {
-    return  yScale(d.name)+20;})
-    .style("style", "label")
-    .text(function(d) {
-    console.log("tackles in rect: " +d.tackles);
-    return d.tackles; });
-
-svg.append("text")
-    .attr("x", (width / 2))
-    .attr("y", (margin.top *4))
-    .attr("text-anchor", "middle")
-    .style("font-size", "16px")
-    .style("text-decoration", "underline")
-    .text("Season Tackles");
-//end interceptions chart
-
+    var width = 500;
+    var height = 500;
+
+    drawChart(topTackles, "tackles", "#tackles-chart", "2017 Season Tackles", width, height);
+    drawChart(topSacks, "sacks", "#sacks-chart", "2017 Season Sacks", width, height);
+    drawChart(topInterceptions, "interceptions", "#interceptions-chart", "2017 Season Interceptions", width, height);
+    drawChart(topRushers, "rushing", "#rushing-chart", "2017 Season Rushing Yards", width, height);
+    drawChart(topReceivers, "receiving", "#receiving-chart", "2017 Season Receiving Yards", width, height);
+    drawChart(topPassers, "passing", "#chart", "2017 Season Passing Yards", width, height);
 }//end make graphs function
+
+
+
+function drawChart(dataArray, field, chartName, chartTitle, width, height){
+
+    // Dimensions
+    var margin = {
+        top: 20,
+        right: 20,
+        bottom: 20,
+        left: 100
+    },
+        width = width,
+        height = height - margin.top - margin.bottom,
+        barHeight = 40,
+        percent = d3.format('%');
+
+    // Create the scale for the axis
+    var xScale = d3.scale.linear()
+        .range([0, width]); // the pixel range to map to
+
+    var yScale = d3.scale.ordinal()
+        .rangeRoundBands([190, height]);
+
+    // Create the axis
+    var xAxis = d3.svg.axis()
+        .scale(xScale)
+        .orient('bottom')
+        .ticks(5);
+
+    var yAxis = d3.svg.axis()
+        .scale(yScale)
+        .orient('left');
+
+
+    xScale.domain([0, dataArray[0][field]]);
+    yScale.domain(dataArray.map(function (d) {
+        return d.name;
+    }));
+
+    // Render the SVG
+    svg = d3.select(chartName)
+        .append('svg')
+        .attr('height', height + margin.top + margin.bottom)
+        .attr('width', 650)
+        .append('g') // Group the content and add margin
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+    // Render the axis
+    svg.append('g')
+        .attr('class', 'x axis')
+        .attr('transform', 'translate(0,' + height + ')')
+        .call(xAxis);
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .selectAll(".tick text")
+        .call(wrap, yScale.rangeBand());
+
+    // Render the bars
+    rect = svg.selectAll('rect')
+        .data(dataArray)
+        .enter();
+
+        rect.append('rect')
+        .attr('y', function (d) {
+        return yScale(d.name);})
+        .attr('width', function (d) {
+        return xScale(d[field]);})
+        .attr('height', barHeight);
+
+
+    //place stats in rect
+    rect.append("text")
+        .style("fill", "white")
+        .style("font-size", "14px")
+        .attr("dy", ".35em")
+        .attr("x",  function (d) {
+         return  xScale(d[field])-58;})
+        .attr("y", function (d) {
+        return  yScale(d.name)+20;})
+        .style("style", "label")
+        .text(function(d) {
+        return d[field]; });
+
+    svg.append("text")
+        .attr("x", (width / 2))
+        .attr("y", (margin.top *4))
+        .attr("text-anchor", "middle")
+        .style("font-size", "16px")
+        .style("text-decoration", "underline")
+        .text(chartTitle);
+}
+
 
 
 //wrap label function from this blogpost: https://bl.ocks.org/mbostock/7555321
--- app/static/js/team-graph.js
+++ app/static/js/team-graph.js
@@ -20,24 +20,47 @@ function makeTeamGraphs(error, stats) {
         throw error;
     }
 
-    console.log("stats: ");
-    console.log(stats);
-    console.log("end stats");
-
 
     //Create a Crossfilter instance
     var ndx = crossfilter(stats);
     var all = ndx.groupAll();
 
-    var winsDim = ndx.dimension(function (d) {return d["wins"]})
-    var offenseDim = ndx.dimension(function (d) {return d["offenseTotal"]});
-    var defenseDim = ndx.dimension(function (d) {return d["defenseTotal"]});
+    var winsDim = ndx.dimension(function (d) {return d["wins"]});
     var stadiumDim = ndx.dimension(function(d){ return d["stadium-roof"]});
 
+    var offenseDim = ndx.dimension(function (d) {return d["offenseTotal"]});
+    var offensePassingDim = ndx.dimension(function (d) {return d["offensePassing"]});
+    var offenseRushingDim = ndx.dimension(function (d) {return d["offenseRushing"]});
+    var offenseReceivingDim = ndx.dimension(function (d) {return d["offenseReceiving"]});
+    var defenseDim = ndx.dimension(function (d) {return d["defenseTotal"]});
+    var defensePassingDim = ndx.dimension(function (d) {return d["defensePassing"]});
+    var defenseRushingDim = ndx.dimension(function (d) {return d["defenseRushing"]});
+    var defenseReceivingDim = ndx.dimension(function (d) {return d["defenseReceiving"]});
+    var fansAvgPerGameDim = ndx.dimension(function (d) {return d["fansAvgPerGame"]});
+    var fansSeasonTotalDim = ndx.dimension(function (d) {return d["fansSeasonTotal"]});
+
+    //the two reactive graphs
     var winsDimGroup = winsDim.group();
-    var offenseDimGroup = offenseDim.group();
-    var defenseDimGroup = defenseDim.group();
     var stadiumGroup = stadiumDim.group();
+
+    var topOffenseTotal = offenseDim.top(10);
+    var topOffensePassing = offensePassingDim.top(10);
+    var topOffenseRushing = offenseRushingDim.top(10);
+    var topOffenseReceiving = offenseReceivingDim.top(10);
+    var topFansAvgPerGame = fansAvgPerGameDim.top(10);
+    var topFansSeasonTotalDim = fansSeasonTotalDim.top(10);
+
+
+    var bottomDefenseTotal = defenseDim.top(Infinity);
+    var bottomDefensePassing = defensePassingDim.top(Infinity);
+    var bottomDefenseRushing = defenseRushingDim.top(Infinity);
+    var bottomDefenseReceiving = defenseReceivingDim.top(Infinity);
+
+    //lower is better; so we have to splice the last 10 off the list for defensive stats
+    var topDefenseTotal = bottomDefenseTotal.splice(22,10);
+    var topDefensePassing = bottomDefensePassing.splice(22,10);
+    var topDefenseRushing = bottomDefenseRushing.splice(22,10);
+    var topDefenseReceiving = bottomDefenseReceiving.splice(22,10);
 
     var winsChart = dc.rowChart("#wins-chart")
         .height(400)
@@ -53,6 +76,23 @@ function makeTeamGraphs(error, stats) {
         .dimension(stadiumDim)
         .group(stadiumGroup);
 
+    var width = 500;
+    var height = 700;
+
+    drawChart(topOffenseTotal, "offenseTotal", "#offense-team-total-chart", "2017 Top 10 Season Avg Yards Offense", width, height);
+    drawChart(topOffensePassing, "offensePassing", "#offense-team-passing-chart", "2017 Top 10 Season Avg Yards Passing", width, height);
+    drawChart(topOffenseRushing, "offenseRushing", "#offense-team-rushing-chart", "2017 Top 10 Season Avg Yards Rushing", width, height);
+    drawChart(topOffenseReceiving, "offenseReceiving", "#offense-team-receiving-chart", "2017 Top 10 Season Avg Yards Receiving", width, height);
+
+    drawChart(topDefenseTotal, "defenseTotal", "#defense-team-total-chart", "2017 Top 10 Season Avg Yards Against", width, height);
+    drawChart(topDefensePassing, "defensePassing", "#defense-team-passing-chart", "2017 Top 10 Season Avg Passing Yards Against", width, height);
+    drawChart(topDefenseRushing, "defenseRushing", "#defense-team-rushing-chart", "2017 Top 10 Season Avg Rushing Yards Against", width, height);
+    drawChart(topDefenseReceiving, "defenseReceiving", "#defense-team-receiving-chart", "2017 Top 10 Season Avg Receiving Yards Against", width, height);
+    drawChart(topFansAvgPerGame, "fansAvgPerGame", "#fans-avg-chart", "2017 Season Top 10 Avg Fans Per Game", width, height);
+    drawChart(topFansSeasonTotalDim, "fansSeasonTotal", "#fans-total-chart", "2017 Top 10 Season Total Fans at Games", width, height);
+
     dc.renderAll();
     AddXAxis(winsChart, "Number of Teams")
+
+
 }
